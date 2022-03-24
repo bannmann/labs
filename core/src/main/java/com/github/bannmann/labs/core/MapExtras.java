998200b2289b7e237ad21bbd6b8091412c395469
Jens Bannmann <jens.b@web.de>
2022-03-24 05:14:33 +0100

    add MapExtras.tryRemove()

--- core/src/main/java/com/github/bannmann/labs/core/MapExtras.java
+++ core/src/main/java/com/github/bannmann/labs/core/MapExtras.java
@@ -10,9 +10,28 @@ import com.github.bannmann.labs.annotations.UpstreamCandidate;
 @UtilityClass
 public class MapExtras
 {
+    /**
+     * Gets the value to which the given key is mapped, if existing. <br>
+     * <br>
+     * Unlike {@link Map#get(Object)}, with this method the compiler verifies type of the key argument. Also, as it
+     * returns an {@link Optional}, calling code can use chained method calls instead of separate {@code if} blocks.
+     *
+     * @return a present {@link Optional} if the given map contains a non-{@code null} value for the given {@code key}.
+     */
     @UpstreamCandidate("Mizool")
     public <K, V> Optional<V> tryGet(Map<K, V> map, K key)
     {
         return Optional.ofNullable(map.get(key));
+    }
+
+    /**
+     * Removes a mapping for the given key, if one exists.
+     *
+     * @return a present {@link Optional} of the value associated with the given {@code key}, if one existed.
+     */
+    @UpstreamCandidate("Mizool")
+    public <K, V> Optional<V> tryRemove(Map<K, V> map, K key)
+    {
+        return Optional.ofNullable(map.remove(key));
     }
 }
--- core/src/test/java/com/github/bannmann/labs/core/TestMapExtras.java
+++ core/src/test/java/com/github/bannmann/labs/core/TestMapExtras.java
@@ -3,9 +3,12 @@ package com.github.bannmann.labs.core;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -37,5 +40,31 @@ public class TestMapExtras
     void testTryGetNullMap()
     {
         assertThatThrownBy(() -> MapExtras.tryGet(null, "key")).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test(dataProvider = "tryRemoveTestData")
+    @SuppressWarnings({ "OptionalUsedAsFieldOrParameterType", "unused" })
+    public void testTryRemove(String testName, Map<String, Integer> map, String key, Optional<Integer> expectedResult)
+    {
+        Optional<Integer> actual = MapExtras.tryRemove(map, key);
+        assertThat(actual).isEqualTo(expectedResult);
+        assertThat(map).doesNotContainKey(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    @DataProvider
+    private Object[][] tryRemoveTestData()
+    {
+        return Arrays.stream(tryGetTestData())
+            .peek(objects -> objects[1] = new HashMap<>((Map<String, Integer>) objects[1]))
+            .collect(Collectors.toList())
+            .toArray(new Object[][]{});
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    void testTryRemoveNullMap()
+    {
+        assertThatThrownBy(() -> MapExtras.tryRemove(null, "key")).isInstanceOf(NullPointerException.class);
     }
 }
