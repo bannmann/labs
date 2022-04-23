54770bf16c48e105a5f1017b0317b12b8a968286
Jens Bannmann <jens.b@web.de>
2022-04-23 13:48:39 +0200

    Error handling now reliably overrides Jersey mappers

--- kumuluzee-errorhandling/src/main/java/com/github/bannmann/labs/kumuluzee/errorhandling/jackson/jaxrs/MizoolJsonMappingExceptionMapper.java
+++ kumuluzee-errorhandling/src/main/java/com/github/bannmann/labs/kumuluzee/errorhandling/jackson/jaxrs/MizoolJsonMappingExceptionMapper.java
@@ -1,8 +1,12 @@
 package com.github.bannmann.labs.kumuluzee.errorhandling.jackson.jaxrs;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.bannmann.labs.kumuluzee.errorhandling.jaxrs.AbstractMizoolExceptionMapper;
 
+@Priority(Priorities.ENTITY_CODER + 761)
 public class MizoolJsonMappingExceptionMapper extends AbstractMizoolExceptionMapper<JsonMappingException>
 {
 }
