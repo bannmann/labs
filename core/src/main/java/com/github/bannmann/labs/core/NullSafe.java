a9c0a290a8e7f37dba7fcb2e11fba852a372cb08
Jens Bannmann <jens.b@web.de>
2023-05-09 08:42:02 +0200

    support static imports of NullSafe methods; rename .get() to .tryGet()

--- core/src/main/java/com/github/bannmann/labs/core/NullSafe.java
+++ core/src/main/java/com/github/bannmann/labs/core/NullSafe.java
@@ -12,7 +12,7 @@ import com.github.bannmann.labs.annotations.UpstreamCandidate;
 @UtilityClass
 public class NullSafe
 {
-    public <A, R> R get(A a, Function<A, R> getter1)
+    public static <A, R> R tryGet(A a, Function<A, R> getter1)
     {
         if (a == null)
         {
@@ -22,9 +22,9 @@ public class NullSafe
         return getter1.apply(a);
     }
 
-    public <A, B, R> R get(A a, Function<A, B> getter1, Function<B, R> getter2)
+    public static <A, B, R> R tryGet(A a, Function<A, B> getter1, Function<B, R> getter2)
     {
-        var b = get(a, getter1);
+        var b = tryGet(a, getter1);
         if (b == null)
         {
             return null;
@@ -33,9 +33,9 @@ public class NullSafe
         return getter2.apply(b);
     }
 
-    public <A, B, C, R> R get(A a, Function<A, B> getter1, Function<B, C> getter2, Function<C, R> getter3)
+    public static <A, B, C, R> R tryGet(A a, Function<A, B> getter1, Function<B, C> getter2, Function<C, R> getter3)
     {
-        var c = get(a, getter1, getter2);
+        var c = tryGet(a, getter1, getter2);
         if (c == null)
         {
             return null;
@@ -44,10 +44,10 @@ public class NullSafe
         return getter3.apply(c);
     }
 
-    public <A, B, C, D, R> R get(
+    public static <A, B, C, D, R> R tryGet(
         A a, Function<A, B> getter1, Function<B, C> getter2, Function<C, D> getter3, Function<D, R> getter4)
     {
-        var d = get(a, getter1, getter2, getter3);
+        var d = tryGet(a, getter1, getter2, getter3);
         if (d == null)
         {
             return null;
@@ -56,7 +56,7 @@ public class NullSafe
         return getter4.apply(d);
     }
 
-    public <A, B, C, D, E, R> R get(
+    public static <A, B, C, D, E, R> R tryGet(
         A a,
         Function<A, B> getter1,
         Function<B, C> getter2,
@@ -64,7 +64,7 @@ public class NullSafe
         Function<D, E> getter4,
         Function<E, R> getter5)
     {
-        var e = get(a, getter1, getter2, getter3, getter4);
+        var e = tryGet(a, getter1, getter2, getter3, getter4);
         if (e == null)
         {
             return null;
@@ -73,7 +73,7 @@ public class NullSafe
         return getter5.apply(e);
     }
 
-    public <A, B, C, D, E, F, R> R get(
+    public static <A, B, C, D, E, F, R> R tryGet(
         A a,
         Function<A, B> getter1,
         Function<B, C> getter2,
@@ -82,7 +82,7 @@ public class NullSafe
         Function<E, F> getter5,
         Function<F, R> getter6)
     {
-        var f = get(a, getter1, getter2, getter3, getter4, getter5);
+        var f = tryGet(a, getter1, getter2, getter3, getter4, getter5);
         if (f == null)
         {
             return null;
@@ -91,7 +91,7 @@ public class NullSafe
         return getter6.apply(f);
     }
 
-    public <T> T coalesce(T t1, T t2)
+    public static <T> T coalesce(T t1, T t2)
     {
         return t1 != null
             ? t1
@@ -99,11 +99,79 @@ public class NullSafe
     }
 
     @SafeVarargs
-    public <T> T coalesce(T... t)
+    public static <T> T coalesce(T... t)
     {
         return Stream.of(t)
             .filter(Objects::nonNull)
             .findFirst()
             .orElse(null);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, R> R get(A a, Function<A, R> getter1)
+    {
+        return tryGet(a, getter1);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, B, R> R get(A a, Function<A, B> getter1, Function<B, R> getter2)
+    {
+        return tryGet(a, getter1, getter2);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function, Function, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, B, C, R> R get(A a, Function<A, B> getter1, Function<B, C> getter2, Function<C, R> getter3)
+    {
+        return tryGet(a, getter1, getter2, getter3);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function, Function, Function, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, B, C, D, R> R get(
+        A a, Function<A, B> getter1, Function<B, C> getter2, Function<C, D> getter3, Function<D, R> getter4)
+    {
+        return tryGet(a, getter1, getter2, getter3, getter4);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function, Function, Function, Function, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, B, C, D, E, R> R get(
+        A a,
+        Function<A, B> getter1,
+        Function<B, C> getter2,
+        Function<C, D> getter3,
+        Function<D, E> getter4,
+        Function<E, R> getter5)
+    {
+        return tryGet(a, getter1, getter2, getter3, getter4, getter5);
+    }
+
+    /**
+     * @deprecated Renamed to {@link #tryGet(Object, Function, Function, Function, Function, Function, Function)}
+     */
+    @Deprecated(forRemoval = true)
+    public static <A, B, C, D, E, F, R> R get(
+        A a,
+        Function<A, B> getter1,
+        Function<B, C> getter2,
+        Function<C, D> getter3,
+        Function<D, E> getter4,
+        Function<E, F> getter5,
+        Function<F, R> getter6)
+    {
+        return tryGet(a, getter1, getter2, getter3, getter4, getter5, getter6);
     }
 }
