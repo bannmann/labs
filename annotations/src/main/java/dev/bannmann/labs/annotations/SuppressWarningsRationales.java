b7a42c39c42c12a42132fdab0bd6504472902b50
Jens Bannmann <jens.b@web.de>
2024-07-19 20:30:01 +0200

    Make SuppressWarningsRationale more flexible

--- annotations/src/main/java/dev/bannmann/labs/annotations/SuppressWarningsRationales.java
+++ annotations/src/main/java/dev/bannmann/labs/annotations/SuppressWarningsRationales.java
@@ -9,30 +9,12 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-/**
- * Documents the author's rationale for using {@link SuppressWarnings}. This is superior to using inline comments or
- * Javadoc comments because it is more obviously linked to the other annotation.
- */
 @Target({ TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, MODULE })
 @Retention(SOURCE)
-@Repeatable(SuppressWarningsRationales.class)
-public @interface SuppressWarningsRationale
+public @interface SuppressWarningsRationales
 {
-    /**
-     * The name of the warning which is suppressed. Could be left empty if obvious.
-     *
-     * @return the name of the warning which is suppressed
-     */
-    String name() default "";
-
-    /**
-     * A string explaining the reason for suppressing the warning.
-     *
-     * @return the rationale
-     */
-    String value();
+    SuppressWarningsRationale[] value();
 }
