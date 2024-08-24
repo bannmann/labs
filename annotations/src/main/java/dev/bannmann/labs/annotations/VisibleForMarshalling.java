60bfc86100618c9575b7cc461caa6db281d0d604
Jens Bannmann <jens.b@web.de>
2024-08-24 20:24:30 +0200

    Allow annotating fields and methods with @VisibleForMarshalling

--- annotations/src/main/java/dev/bannmann/labs/annotations/VisibleForMarshalling.java
+++ annotations/src/main/java/dev/bannmann/labs/annotations/VisibleForMarshalling.java
@@ -1,17 +1,19 @@
 package dev.bannmann.labs.annotations;
 
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.Target;
 
 /**
- * Annotates an element that was made public only to make it work with marshalling, such as to/from JSON.<br>
+ * Annotates an element whose visibility was relaxed to make it work with marshalling, such as to/from JSON.<br>
  * <br>
  * Note: the name of this annotation intentionally mimics Guava's
  * {@link com.google.common.annotations.VisibleForTesting}.
  */
-@Target({ TYPE, CONSTRUCTOR })
+@Target({ TYPE, FIELD, METHOD, CONSTRUCTOR })
 public @interface VisibleForMarshalling
 {
 }
