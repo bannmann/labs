258ef80f3a7171128933ba334454f15fb04cca21
Jens Bannmann <jens.b@web.de>
2025-06-20 23:27:00 +0200

    Add readX() shortcut methods to JsonNav

--- json-nav/nav-core/src/main/java/dev/bannmann/labs/json_nav/NumberRef.java
+++ json-nav/nav-core/src/main/java/dev/bannmann/labs/json_nav/NumberRef.java
@@ -10,11 +10,55 @@ import com.google.errorprone.annotations.Immutable;
 @Immutable
 public abstract non-sealed class NumberRef extends TypedRef
 {
+    /**
+     * @see #readInteger()
+     */
     public abstract Value<Integer> intoInteger();
 
+    /**
+     * @see #readLong()
+     */
     public abstract Value<Long> intoLong();
 
+    /**
+     * @see #readDouble()
+     */
     public abstract Value<Double> intoDouble();
 
+    /**
+     * @see #readBigDecimal()
+     */
     public abstract Value<BigDecimal> intoBigDecimal();
+
+    /**
+     * @see #intoInteger()
+     */
+    public int readInteger()
+    {
+        return intoInteger().read();
+    }
+
+    /**
+     * @see #intoLong()
+     */
+    public long readLong()
+    {
+        return intoLong().read();
+    }
+
+    /**
+     * @see #intoDouble()
+     */
+    public double readDouble()
+    {
+        return intoDouble().read();
+    }
+
+    /**
+     * @see #intoBigDecimal()
+     */
+    public BigDecimal readBigDecimal()
+    {
+        return intoBigDecimal().read();
+    }
 }
