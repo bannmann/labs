e888dd5d1eb7a67d1187243f7684c833d217c4c3
Jens Bannmann <jens.b@web.de>
2025-08-09 15:15:50 +0200

    Fix sonar violation

--- watchdog-maven-plugin/src/main/java/dev/bannmann/labs/watchdog/DelegatingPlexusLogger.java
+++ watchdog-maven-plugin/src/main/java/dev/bannmann/labs/watchdog/DelegatingPlexusLogger.java
@@ -5,9 +5,13 @@ import lombok.experimental.Delegate;
 
 import org.codehaus.plexus.logging.Logger;
 
+import dev.bannmann.labs.annotations.SuppressWarningsRationale;
+
 @RequiredArgsConstructor
 abstract class DelegatingPlexusLogger implements Logger
 {
     @Delegate
+    @SuppressWarnings("java:S1312")
+    @SuppressWarningsRationale("The usual name 'log' would detract from the fact that this is a delegation target and the sole purpose of this class")
     protected final Logger target;
 }
